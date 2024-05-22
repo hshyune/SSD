90d279733ad50912c6ed4f3d1bbe6d24c70e135e
jsryoo <jspooh1989@naver.com>
2024-05-22 13:44:51 +0900

    IIoInterface 추가, SSD Mock 추가

--- SSD/SSD.cpp
+++ SSD/SSD.cpp
@@ -1,9 +1,31 @@
+
+class IIoInterface {
+	virtual int read() = 0;
+	virtual void write() = 0;
+};
+
+class Nand : public IIoInterface {
+	// IIoInterface을(를) 통해 상속됨
+	int read() override
+	{
+		return 0;
+	}
+	void write() override
+	{
+	}
+};
+
 class SSD {
 public:
+	SSD(IIoInterface* nand) {
+		this->nand = nand;
+	}
 	int read(int address) {
 		return 0;
 	}
 	void write(int address, int data) {
 		return;
 	}
+private:
+	IIoInterface* nand;
 };
--- SSDTest/test.cpp
+++ SSDTest/test.cpp
@@ -1,15 +1,30 @@
-#include "pch.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include "../SSD/SSD.cpp"
 
-TEST(TestCaseName, TestName) {
+using namespace std;
+using namespace testing;
+
+class IoInterfaceMock : public IIoInterface {
+public:
+	MOCK_METHOD(int, read, (), (override));
+	MOCK_METHOD(void, write, (), (override));
+};
+
+class SSDTest : public testing::Test {
+public:
+	IoInterfaceMock mock;
+	SSD ssd{ &mock };
+};
+
+TEST_F(SSDTest, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
 
-TEST(SSD_READ, TEST1) {
-	SSD* ssd = new SSD();
+TEST_F(SSDTest, TEST1) {
 	int address = 1;
-	int ret = ssd->read(1);
+	int ret = ssd.read(1);
 	EXPECT_EQ(ret, 0);
 }
 
