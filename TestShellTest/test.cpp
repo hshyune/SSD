6b60e3628774108bfd1f7c88f4b2cb94960d3299
DESKTOP-UI8BHDH\user <hyun3164@gmail.com>
2024-05-22 13:50:13 +0900

    method_mock 추가

--- TestShellTest/test.cpp
+++ TestShellTest/test.cpp
@@ -1,6 +1,21 @@
-#include "pch.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../TestShell/TestShell.cpp"
+
+using namespace std;
+using namespace testing;
+
+class TestShellMock : public TestShell {
+	MOCK_METHOD(void, write, (int address, int data), ());
+	MOCK_METHOD(int, read, (int address), ());
+	MOCK_METHOD(void, exit, (int address, int data), ());
+	MOCK_METHOD(void, help, (int address, int data), ());
+	MOCK_METHOD(void, fullwrite, (int data), ());
+	MOCK_METHOD(void, fullread, (), ());
+};
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
