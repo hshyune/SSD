7cdc0f20e5c5bff414bc1eceaa25dbb5962dcc6c
lss <cjswosuny01@naver.com>
2024-05-22 14:15:54 +0900

    Add testsheel basic read testcases

--- TestShellTest/test.cpp
+++ TestShellTest/test.cpp
@@ -8,7 +8,7 @@ using namespace testing;
 class TestShellMock : public TestShell {
 public:
 	MOCK_METHOD(void, write, (int address, int data), ());
-	MOCK_METHOD(int, read, (int address), ());
+	MOCK_METHOD(int, read, (int address, int times), ());
 	MOCK_METHOD(void, exit, (int address, int data), ());
 	MOCK_METHOD(void, help, (int address, int data), ());
 	MOCK_METHOD(void, fullwrite, (int data), ());
@@ -21,3 +21,25 @@ TEST(TestCaseName, TEST_SHELL_EXIT_CALL) {
 	EXPECT_CALL(mock, exit).Times(1);
 }
 
+TEST(TestCaseName, TEST_SHELL_READ_UNWRITTEN_LBA) {
+	TestShellMock mock;
+
+	int address = 0x0;
+	int times = 3;
+	int expectedData = 0x0;
+	int resultData = mock.read(address, times);
+
+	EXPECT_EQ(expectedData, resultData);
+}
+
+TEST(TestCaseName, TEST_SHELL_READ_WRITTEN_LBA) {
+	TestShellMock mock;
+
+	int address = 0x0;
+	int times = 3;
+	int expectedData = 0x12345678;
+	mock.write(address, expectedData);
+	int resultData = mock.read(address, times);
+
+	EXPECT_EQ(expectedData, resultData);
+}
