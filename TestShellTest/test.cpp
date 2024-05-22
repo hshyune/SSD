c46a65941addc1a701ef76624e60f47a08194379
DESKTOP-UI8BHDH\user <hyun3164@gmail.com>
2024-05-22 14:02:20 +0900

    exit help method TC 추가

--- TestShellTest/test.cpp
+++ TestShellTest/test.cpp
@@ -9,8 +9,8 @@ class TestShellMock : public TestShell {
 public:
 	MOCK_METHOD(void, write, (int address, int data), ());
 	MOCK_METHOD(int, read, (int address), ());
-	MOCK_METHOD(void, exit, (int address, int data), ());
-	MOCK_METHOD(void, help, (int address, int data), ());
+	MOCK_METHOD(void, exit, (), ());
+	MOCK_METHOD(void, help, (), ());
 	MOCK_METHOD(void, fullwrite, (int data), ());
 	MOCK_METHOD(void, fullread, (), ());
 };
@@ -19,5 +19,15 @@ TEST(TestCaseName, TEST_SHELL_EXIT_CALL) {
 	TestShellMock mock;
 
 	EXPECT_CALL(mock, exit).Times(1);
+
+	mock.exit();
+}
+
+TEST(TestCaseName, TEST_SHELL_EXIT_HELP) {
+	TestShellMock mock;
+
+	EXPECT_CALL(mock, help).Times(1);
+
+	mock.help();
 }
 
