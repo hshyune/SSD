681c3bacc128a856ed38fd45eb63648d7d0c5358
lss <cjswosuny01@naver.com>
2024-05-23 10:15:45 +0900

    Custom exception cpp 추가

---
+++ TestShellTest/Exception.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+class ExceptionInvalidCommand : public exception
+{
+public:
+	char const* what() const override
+	{
+		"INVALID COMMAND";
+	}
+};
+
+class ExceptionLbaOutOfRange : public exception
+{
+public:
+	char const* what() const override
+	{
+		"LBA 범위는 0~99 입니다.";
+	}
+};
+
+class ExceptionInvalidData : public exception
+{
+public:
+	char const* what() const override
+	{
+		"Data의 1자리당 범위는 16진수로 0~F 입니다.";
+	}
+};
+
+class ExceptionInvalidParameter : public exception
+{
+public:
+	char const* what() const override
+	{
+		"파라미터 Format이 정상적이지 않습니다.";
+	}
+};
